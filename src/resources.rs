fbc9dbb7e3e82054a87751b097c2d7b1d1789e47
psobczak <pawel.sobczak@rst.com.pl>
2024-10-17 12:20:28 +0200

    Better resources

--- src/resources.rs
+++ src/resources.rs
@@ -13,9 +13,49 @@ impl Plugin for ResourcePlugin {
 
 #[derive(Component, Debug)]
 pub enum Resource {
-    Treasure,
     Gold,
     Crystals,
+    Mercury,
+    Sulfur,
+    Ore,
+    Wood,
+    Gems,
+}
+
+#[derive(Bundle, Debug)]
+pub struct ResourceBundle {
+    resource: Resource,
+    sprite_bundle: SpriteBundle,
+    texture_atlas: TextureAtlas,
+    sprite_sheet_animation: SpritesheetAnimation,
+}
+
+impl ResourceBundle {
+    fn new(
+        resource: Resource,
+        handles: &GameAssetsHandles,
+        animations: &AnimationLibrary,
+    ) -> Option<Self> {
+        let animation_id = match resource {
+            Resource::Gold => animations.animation_with_name("gold_blink")?,
+            Resource::Crystals => animations.animation_with_name("crystals_blink")?,
+            Resource::Gems => animations.animation_with_name("gems_blink")?,
+            Resource::Mercury => animations.animation_with_name("mercury")?,
+            Resource::Sulfur => animations.animation_with_name("sulfur")?,
+            Resource::Ore => animations.animation_with_name("ore")?,
+            Resource::Wood => animations.animation_with_name("wood")?,
+        };
+
+        Some(Self {
+            resource,
+            sprite_bundle: SpriteBundle {
+                texture: handles.resources.clone(),
+                ..Default::default()
+            },
+            texture_atlas: TextureAtlas::from(handles.resources_layout.clone()),
+            sprite_sheet_animation: SpritesheetAnimation::from_id(animation_id),
+        })
+    }
 }
 
 fn spawn_treasure(
@@ -23,13 +63,9 @@ fn spawn_treasure(
     handles: Res<GameAssetsHandles>,
     animations: Res<AnimationLibrary>,
 ) {
-    commands.spawn((
-        Resource::Gold,
-        SpriteBundle {
-            texture: handles.resources.clone(),
-            ..Default::default()
-        },
-        TextureAtlas::from(handles.resources_layout.clone()),
-        SpritesheetAnimation::from_id(animations.animation_with_name("jewels_blink").unwrap()),
-    ));
+    let Some(bundle) = ResourceBundle::new(Resource::Crystals, &handles, &animations) else {
+        return error!("Failed to create resource bundle");
+    };
+
+    commands.spawn(bundle);
 }
